36a8fac5d37c22171d116247641d7200bd5af741
Alexander Belousov <pegas206@mail.ru>
2022-12-21 17:29:52 +0700

    Задача 1 (обязательная)

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,45 @@
+import ru.netology.loremIpsum.Text;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Objects;
+
 public class Main {
+    public static void main(String[] args) {
+        //Переводим текст статьи в набор символов
+        char[] kitSymbols = Text.Statya.toLowerCase(Locale.ROOT).toCharArray();
+
+        //Завели мапу
+        Map<Character, Integer> symbols = new HashMap<>();
+        for (char symbol : kitSymbols) {
+            if (Character.isLetter(symbol)) {
+                Integer integer = symbols.get(symbol);
+                if (integer == null) {
+                    symbols.put(symbol, 1);
+                } else symbols.put(symbol, ++integer);
+            }
+        }
+        //Максимальное число повторения символа
+        Map.Entry<Character, Integer> maxEntry = null;
+        for (Map.Entry<Character, Integer> entry : symbols.entrySet()) {
+            if (maxEntry == null || entry.getValue() > maxEntry.getValue()) {
+                maxEntry = entry;
+
+            }
+        }
+        System.out.println("Максимальное значение у " + maxEntry);
+
+        //Минимальное число повторения символа
+        Map.Entry<Character, Integer> minEntry = null;
+        for (Map.Entry<Character, Integer> entry : symbols.entrySet()) {
+            if (minEntry == null || entry.getValue() < minEntry.getValue()) {
+                minEntry = entry;
+
+            }
+        }
+        System.out.println("Минимальное значение у " + minEntry);
+        System.out.println(symbols);
+    }
+
 }
